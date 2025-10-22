4716d82e208faab606c6d73206fc72d4292656dc
WillZhang <zwillthink@outlook.com>
2025-10-22 15:32:02 +0800

    refactor: simplify imports and clean up style.css by removing unused comments

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,7 +4,8 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 // Import Sphere UI components and styles
-import SphereUI from '../../../src/index'
+import SphereUI from '../../../src'
+import '../../../src/style.css'
 // 通过字符串形式引入样式，确保在构建时正确处理
 
 export default {
