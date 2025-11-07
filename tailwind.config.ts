48e6f264d9ecbed371e13c10747c301e8f5b967d
WillZhang <zwillthink@outlook.com>
2025-11-07 18:49:24 +0800

    chore(tailwind): remove tailwind.config.ts file

--- tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-  darkMode: 'class', // 使用 class 策略
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-} satisfies Config
